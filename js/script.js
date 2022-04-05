37883f0c53e6543d254eeaa50a4e46146fabe444
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2022-04-04 23:28:36 -0500

    fixed my mistake

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ function init(){
 			var txt = document.getElementById("entryinput").value;
 			alert("Dinora Saldivar:" + txt);
 			document.getElementById("textoutput").innerHTML = txt}
-			var button = document.getElementById("entrybutton");
-			button.addEventListener('click', alertFunction()
+			var button = document.getElementById('entrybutton');
+			button.addEventListener('click', alertFunction);
 			
 }
 window.addEventListener('load', init);
