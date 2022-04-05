d8a4b5226bef76cbfb948e2b180a15c3266a2560
Dinora Saldivar <dsaldivar@hawk.iit.edu>
2022-04-04 23:24:17 -0500

    checked

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function init(){
+		function alertFunction(){
+			var txt = document.getElementById("entryinput").value;
+			alert("Dinora Saldivar:" + txt);
+			document.getElementById("textoutput").innerHTML = txt}
+			var button = document.getElementById("entrybutton");
+			button.addEventListener('click', alertFunction()
+			
+}
+window.addEventListener('load', init);
